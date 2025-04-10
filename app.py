5d824b42c7ce9c7f0bcfbebadc83728b9c8235f6
nicosnow <zeroxnqx@gmail.com>
2025-04-10 11:36:08 +0200

    index, json setup

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+# Load blog posts from JSON file
+with open('blog_posts.json', 'r') as file:
+    blog_posts = json.load(file)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    return render_template('index.html', title='Home', posts=blog_posts)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5500, debug=True)
